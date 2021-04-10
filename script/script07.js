f8617a91f41e030a50fbd19a1d938fdf6a3c89fc
shju0317 <shju0317@naver.com>
2021-04-10 22:02:42 +0900

    menu05

---
+++ script/script07.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(".nav > ul > li").hover(
+	function(){
+		$(this).find(".submenu").stop().slideDown();
+	},
+	function(){
+		$(this).find(".submenu").stop().slideUp();
+	}
+)
