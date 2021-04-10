b12a449e58130cc74991c20f7ff9318fc7b75962
shju0317 <shju0317@naver.com>
2021-04-10 15:58:02 +0900

    menu03

---
+++ script/script05.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(".nav > ul >li").mouseover(function(){
+	$(".nav > ul >li > ul").show();
+});
+
+$(".nav > ul >li").mouseleave(function(){
+	$(".nav > ul >li > ul").hide();
+});
